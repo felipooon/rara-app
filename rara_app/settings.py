196b8afcd002402f2ba04668f7ac749a50ffe223
Felipe Godoy <felipegp90@gmail.com>
2026-04-14 10:15:54 -0400

    Primer commit - base tienda funcional con panel

--- rara_app/settings.py
+++ rara_app/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c5jsasag*nn((bmld%b05orxp@be=w36(lh15!5ubmt6%@&&u+'
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
